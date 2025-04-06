49269dbcd8a887c8f51b6ce60e506573c1d545aa
Rui Rua <rui.rrua@gmail.com>
2025-04-06 16:47:22 +0400

    fix

--- src/utils/csvLoader.js
+++ src/utils/csvLoader.js
@@ -57,7 +57,7 @@ export const fetchCodeExample = async (exampleFileName) => {
   }
   
   try {
-    const response = await fetch(`/android-performance-issues/${exampleFileName}`);
+    const response = await fetch(`/android-performance-issues/data/examples/${exampleFileName}`);
     if (!response.ok) {
       return `Code example could not be loaded (${exampleFileName})`;
     }
